28aff3c53e9f950aa9fa5fdab20e357774a4b040
Joshua Plicque <plicjo@gmail.com>
2020-09-03 16:00:46 -0400

    Add technologies

--- src/App.svelte
+++ src/App.svelte
@@ -361,7 +361,7 @@
           <div class="col-12 col-md-5 col-lg-6 order-md-2">
 
             <!-- Image -->
-            <img src="/img/patient_pilot_award.jpg" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="...">
+            <img src="/img/patient_pilot_award.png" class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="...">
 
           </div>
           <div class="col-12 col-md-7 col-lg-6 order-md-1">
@@ -393,57 +393,78 @@
       <section class="py-8 py-md-11 border-bottom">
         <div class="container">
           <div class="row">
-            <div class="col-12 col-md-4">
+            <div class="col-12 col-md-3">
 
-              <!-- Icon -->
-              <div class="icon text-primary mb-3">
-                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M7 3h10a4 4 0 110 8H7a4 4 0 110-8zm0 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA"/><path d="M7 13h10a4 4 0 110 8H7a4 4 0 110-8zm10 6a2 2 0 100-4 2 2 0 000 4z" fill="#335EEA" opacity=".3"/></g></svg>
-              </div>
 
               <!-- Heading -->
               <h3>
-                Built for developers
+                Realtime
               </h3>
+
+              <!-- Icon -->
+              <div class="icon text-primary mb-3">
+                <img src="/img/phoenix.png" class="img-fluid mh-40" alt="Realtime with Phoenix">
+              </div>
 
               <!-- Text -->
               <p class="text-muted mb-6 mb-md-0">
-                Landkit is built to make your life easier. Variables, build tooling, documentation, and reusable components.
+                The Elixir language's Phoenix framework is the epitome of cutting edge excellence. We build highly interactive realtime web applications using the same technology of our nation's rock solid telecomm network.
               </p>
 
             </div>
-            <div class="col-12 col-md-4">
-
-              <!-- Icon -->
-              <div class="icon text-primary mb-3">
-                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M5.5 4h4A1.5 1.5 0 0111 5.5v1A1.5 1.5 0 019.5 8h-4A1.5 1.5 0 014 6.5v-1A1.5 1.5 0 015.5 4zm9 12h4a1.5 1.5 0 011.5 1.5v1a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-1a1.5 1.5 0 011.5-1.5z" fill="#335EEA"/><path d="M5.5 10h4a1.5 1.5 0 011.5 1.5v7A1.5 1.5 0 019.5 20h-4A1.5 1.5 0 014 18.5v-7A1.5 1.5 0 015.5 10zm9-6h4A1.5 1.5 0 0120 5.5v7a1.5 1.5 0 01-1.5 1.5h-4a1.5 1.5 0 01-1.5-1.5v-7A1.5 1.5 0 0114.5 4z" fill="#335EEA" opacity=".3"/></g></svg>
-              </div>
+            <div class="col-12 col-md-3">
 
               <!-- Heading -->
               <h3>
-                Designed to be modern
+                Rapid Development
               </h3>
+
+              <!-- Icon -->
+              <div class="icon text-primary mb-3">
+                <img src="/img/rails.png" class="img-fluid mh-40" alt="Rapid development with Rails">
+              </div>
 
               <!-- Text -->
               <p class="text-muted mb-6 mb-md-0">
-                Designed with the latest design trends in mind. Landkit feels modern, minimal, and beautiful.
+                The gold standard of getting quickly from idea to market is Ruby on Rails. Twitter, AirBnB, and Hulu all began with small, highly productive teams using this technology.
               </p>
 
             </div>
-            <div class="col-12 col-md-4">
-
-              <!-- Icon -->
-              <div class="icon text-primary mb-3">
-                <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"/><path d="M17.272 8.685a1 1 0 111.456-1.37l4 4.25a1 1 0 010 1.37l-4 4.25a1 1 0 01-1.456-1.37l3.355-3.565-3.355-3.565zm-10.544 0L3.373 12.25l3.355 3.565a1 1 0 01-1.456 1.37l-4-4.25a1 1 0 010-1.37l4-4.25a1 1 0 011.456 1.37z" fill="#335EEA"/><rect fill="#335EEA" opacity=".3" transform="rotate(15 12 12)" x="11" y="4" width="2" height="16" rx="1"/></g></svg>
-              </div>
+            <div class="col-12 col-md-3">
 
               <!-- Heading -->
               <h3>
-                Documentation for everything
+                Web
               </h3>
+
+              <!-- Icon -->
+              <div class="icon text-primary mb-3">
+                <img src="/img/ember.png" class="img-fluid mh-40" alt="Web with Ember">
+              </div>
 
               <!-- Text -->
               <p class="text-muted mb-0">
-                We've written extensive documentation for components and tools, so you never have to reverse engineer anything.
+                Using EmberJS, we opt in to best-in-class all-in-one tooling for building reliable user interaces.
+              </p>
+
+            </div>
+
+            <div class="col-12 col-md-3">
+
+              <!-- Heading -->
+              <h3>
+                Mobile
+              </h3>
+
+
+              <!-- Icon -->
+              <div class="icon text-primary mb-3">
+                <img src="/img/flutter.png" class="img-fluid mh-40" alt="Mobile with Flutter">
+              </div>
+
+              <!-- Text -->
+              <p class="text-muted mb-0">
+                We use Google's Flutter to build cross-platform mobile applications. Yes, it's better than React Native.
               </p>
 
             </div>
@@ -1456,4 +1477,7 @@
     </footer>
 
 <style>
+  .mh-40 {
+    max-height: 40px;
+  }
 </style>
