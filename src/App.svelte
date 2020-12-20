685debe332a3aae0dcfdf0c6308d36c1c6d53a39
Joshua Plicque <plicjo@gmail.com>
2020-12-19 19:30:01 -0500

    Greatly simplify list of offerings

--- src/App.svelte
+++ src/App.svelte
@@ -70,7 +70,7 @@
 
             <!-- Heading -->
             <h1 class="display-3 text-center text-md-left">
-              Award Winning <span class="text-primary">Business Software Development</span><br>
+              Award-Winning <span class="text-primary">Business Software Development</span><br>
             </h1>
 
             <!-- Text -->
@@ -99,14 +99,7 @@
       <section class="py-8 py-md-11 border-bottom">
         <div class="container">
           <div class="row">
-            <div class="col-12 col-md-3">
-
-
-              <!-- Heading -->
-              <h3>
-                Realtime
-              </h3>
-
+            <div class="col-12 col-md-6">
               <!-- Icon -->
               <div class="icon text-primary mb-3">
                 <img src="/img/phoenix.png" class="img-fluid mh-40" alt="Realtime with Phoenix">
@@ -118,13 +111,7 @@
               </p>
 
             </div>
-            <div class="col-12 col-md-3">
-
-              <!-- Heading -->
-              <h3>
-                Rapid Development
-              </h3>
-
+            <div class="col-12 col-md-6">
               <!-- Icon -->
               <div class="icon text-primary mb-3">
                 <img src="/img/rails.png" class="img-fluid mh-40" alt="Rapid development with Rails">
@@ -134,45 +121,6 @@
               <p class="text-muted mb-6 mb-md-0">
                 The gold standard of getting from back-of-the-napkin idea to market quickly is Ruby on Rails. Twitter, AirBnB, Twitch, and Hulu all began with small, highly productive teams using this technology.
               </p>
-
-            </div>
-            <div class="col-12 col-md-3">
-
-              <!-- Heading -->
-              <h3>
-                Web
-              </h3>
-
-              <!-- Icon -->
-              <div class="icon text-primary mb-3">
-                <img src="/img/ember.png" class="img-fluid mh-40" alt="Web with Ember">
-              </div>
-
-              <!-- Text -->
-              <p class="text-muted mb-6 mb-0">
-                Using EmberJS, we opt in to best-in-class all-in-one tooling for building reliable user interaces.
-              </p>
-
-            </div>
-
-            <div class="col-12 col-md-3">
-
-              <!-- Heading -->
-              <h3>
-                Mobile
-              </h3>
-
-
-              <!-- Icon -->
-              <div class="icon text-primary mb-3">
-                <img src="/img/flutter.png" class="img-fluid mh-40" alt="Mobile with Flutter">
-              </div>
-
-              <!-- Text -->
-              <p class="text-muted mb-0">
-                We use Google's Flutter to build cross-platform mobile applications. Yes, it's better than React Native.
-              </p>
-
             </div>
           </div> <!-- / .row -->
         </div> <!-- / .container -->
@@ -240,46 +188,7 @@
             </p>
 
           </div>
-
-          <div class="col-12 col-md-6 text-center aos-init aos-animate" data-aos="fade-up">
-
-            <!-- Icon -->
-            <div class="icon icon-lg mb-4">
-              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M12 11a4 4 0 110-8 4 4 0 010 8z" fill="#335EEA" opacity=".3"></path><path d="M3 20.2c.388-4.773 4.262-7.2 8.983-7.2 4.788 0 8.722 2.293 9.015 7.2.012.195 0 .8-.751.8H3.727c-.25 0-.747-.54-.726-.8z" fill="#335EEA"></path></g></svg>
-            </div>
-
-            <!-- Heading -->
-            <h3 class="font-weight-bold">
-              On-Demand CTO
-            </h3>
-
-            <!-- Text -->
-            <p class="text-muted mb-8">
-              You've got a rapidly growing business. You know you need to leverage technology to grow. We'll help you make all the right decisions for your new infrastructure.
-            </p>
-
-          </div>
-          <div class="col-12 col-md-6 text-center aos-init aos-animate" data-aos="fade-up">
-
-            <!-- Icon -->
-            <div class="icon icon-lg mb-4">
-              <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path d="M8 3v.5A1.5 1.5 0 009.5 5h5A1.5 1.5 0 0016 3.5V3h2a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V5a2 2 0 012-2h2z" fill="#335EEA" opacity=".3"></path><path d="M11 2a1 1 0 012 0h1.5a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-5a.5.5 0 01-.5-.5v-1a.5.5 0 01.5-.5H11z" fill="#335EEA"></path><rect fill="#335EEA" opacity=".3" x="7" y="10" width="5" height="2" rx="1"></rect><rect fill="#335EEA" opacity=".3" x="7" y="14" width="9" height="2" rx="1"></rect></g></svg>
-            </div>
-
-            <!-- Heading -->
-            <h3 class="font-weight-bold">
-              Software Strategy Planning
-            </h3>
-
-            <!-- Text -->
-            <p class="text-muted mb-8">
-              Taking broad ideas and turning them into a concrete software plan is what we specialize in. All of our software projects begin with a detailed plan of attack.
-            </p>
-
-          </div>
-              </div>
-            </div> <!-- / .row -->
-          </section>
+        </section>
 
                 <!-- TESTIMONIALS
                   ================================================== -->
@@ -393,70 +302,8 @@
                                         </footer>
 
                                       </blockquote>
-
                                   </div>
-                                  <div class="w-100">
-                                    <!-- Body -->
-                                    <div class="card-body">
-                                      <blockquote class="blockquote text-center mb-0">
-
-                                        <!-- Brand -->
-                                        <div class="row justify-content-center mb-5 mb-md-7">
-                                          <div class="col-6 col-sm-4 col-md-7 col-lg-5">
-
-                                            <!-- Logo -->
-                                            <div class="img-fluid svg-shim" style="color: #FF5A5F;">
-                                              <a href='https://www.splife.com' target='_blank'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 206.8 77" style="enable-background:new 0 0 206.8 77;" xml:space="preserve" data-ember-extension="1">
-                                                  <style type="text/css">
-                                                    .st0{fill:#f75b5b;}
-                                                  </style>
-                                                  <g>
-                                                  <g>
-                                                  <path class="st0" d="M42.3,53.3H16.1c-0.6,0-1.2-0.5-1.2-1.2s0.5-1.2,1.2-1.2h26.2c1.2,0,2.2-1,2.2-2.2s-1-2.2-2.2-2.2H26.8    c-10.8,0-19.7-8.8-19.7-19.6S15.9,7.2,26.8,7.2h21.1c0.6,0,1.2,0.5,1.2,1.2s-0.5,1.2-1.2,1.2H26.8c-9.6,0-17.3,7.8-17.3,17.3    s7.8,17.3,17.3,17.3h15.5c2.5,0,4.5,2,4.5,4.5S44.8,53.3,42.3,53.3z"/>
-                                                  </g>
-                                                  <g>
-                                                  <path class="st0" d="M42.3,60.8h-34c-0.6,0-1.2-0.5-1.2-1.2c0-0.6,0.5-1.2,1.2-1.2h34c5.4,0,9.8-4.4,9.8-9.8S47.7,39,42.3,39H26.8    c-6.7,0-12.1-5.4-12.1-12.1s5.4-12.1,12.1-12.1h32c0.6,0,1.2,0.5,1.2,1.2c0,0.6-0.5,1.2-1.2,1.2h-32c-5.4,0-9.8,4.4-9.8,9.8    s4.4,9.8,9.8,9.8h15.5c6.7,0,12.1,5.4,12.1,12.1S48.9,60.8,42.3,60.8z"/>
-                                                  </g>
-                                                  <g>
-                                                  <path class="st0" d="M42.3,68.4H15.6c-0.6,0-1.2-0.5-1.2-1.2s0.5-1.2,1.2-1.2h26.7c9.6,0,17.3-7.8,17.3-17.3s-7.8-17.3-17.3-17.3    H26.8c-2.5,0-4.5-2-4.5-4.5s2-4.5,4.5-4.5h24.6c0.6,0,1.2,0.5,1.2,1.2c0,0.6-0.5,1.2-1.2,1.2H26.8c-1.2,0-2.2,1-2.2,2.2    c0,1.2,1,2.2,2.2,2.2h15.5c10.8,0,19.6,8.8,19.6,19.6S53.1,68.4,42.3,68.4z"/>
-                                                  </g>
-                                                  </g>
-                                                  <g>
-                                                  <path class="st0" d="M77.8,46.7c0.4,0.8,0.9,1.4,1.6,1.9c0.7,0.5,1.5,0.8,2.4,1.1s1.8,0.4,2.8,0.4c0.7,0,1.4-0.1,2.1-0.2   c0.8-0.1,1.5-0.3,2.1-0.6c0.7-0.3,1.2-0.7,1.6-1.3c0.4-0.5,0.7-1.2,0.7-2.1c0-0.9-0.3-1.6-0.9-2.2c-0.6-0.6-1.3-1-2.3-1.4   c-0.9-0.4-2-0.7-3.2-1s-2.4-0.6-3.6-0.9c-1.3-0.3-2.5-0.7-3.7-1.1c-1.2-0.5-2.3-1-3.2-1.8c-0.9-0.7-1.7-1.6-2.3-2.7   c-0.6-1.1-0.9-2.4-0.9-3.9c0-1.7,0.4-3.2,1.1-4.5c0.7-1.3,1.7-2.3,2.9-3.2c1.2-0.8,2.5-1.5,4-1.9c1.5-0.4,3-0.6,4.5-0.6   c1.8,0,3.4,0.2,5,0.6c1.6,0.4,3,1,4.3,1.9c1.3,0.9,2.2,2,3,3.4c0.7,1.4,1.1,3,1.1,4.9H90c-0.1-1-0.3-1.8-0.6-2.5   c-0.4-0.7-0.8-1.2-1.4-1.5c-0.6-0.4-1.3-0.6-2-0.8c-0.8-0.2-1.6-0.2-2.5-0.2c-0.6,0-1.2,0.1-1.8,0.2C81,26.7,80.5,27,80,27.3   c-0.5,0.3-0.9,0.7-1.2,1.2c-0.3,0.5-0.5,1.1-0.5,1.8c0,0.7,0.1,1.2,0.4,1.6c0.2,0.4,0.7,0.8,1.5,1.1c0.7,0.3,1.8,0.7,3,1   c1.3,0.3,3,0.8,5.1,1.3c0.6,0.1,1.5,0.4,2.6,0.7c1.1,0.3,2.2,0.9,3.3,1.6c1.1,0.7,2,1.7,2.8,2.9c0.8,1.2,1.2,2.7,1.2,4.6   c0,1.5-0.3,3-0.9,4.3c-0.6,1.3-1.5,2.4-2.7,3.4c-1.2,1-2.6,1.7-4.4,2.2c-1.7,0.5-3.7,0.8-6,0.8c-1.8,0-3.6-0.2-5.4-0.7   c-1.7-0.5-3.3-1.2-4.6-2.1c-1.3-1-2.4-2.2-3.2-3.7c-0.8-1.5-1.2-3.3-1.1-5.3h7.1C77.2,45,77.4,46,77.8,46.7z"/>
-                                                  <path class="st0" d="M109.2,30.7v3.1h0.1c0.8-1.3,1.8-2.3,3.1-2.9c1.2-0.6,2.6-0.9,4.1-0.9c1.9,0,3.5,0.4,4.9,1.1   c1.4,0.7,2.5,1.7,3.4,2.9c0.9,1.2,1.6,2.6,2,4.2c0.4,1.6,0.7,3.2,0.7,4.9c0,1.6-0.2,3.2-0.7,4.7c-0.4,1.5-1.1,2.8-2,4   c-0.9,1.2-2,2.1-3.3,2.8s-2.9,1-4.7,1c-1.5,0-2.9-0.3-4.2-0.9c-1.3-0.6-2.3-1.5-3.1-2.7h-0.1v11.5h-6.7V30.7H109.2z M117.8,50   c0.7-0.4,1.3-1,1.8-1.7c0.5-0.7,0.8-1.5,1-2.5c0.2-0.9,0.3-1.9,0.3-2.9c0-1-0.1-1.9-0.3-2.9c-0.2-0.9-0.5-1.8-1-2.5   c-0.5-0.7-1.1-1.3-1.8-1.8c-0.7-0.5-1.6-0.7-2.7-0.7c-1.1,0-2,0.2-2.7,0.7c-0.7,0.5-1.3,1-1.8,1.8c-0.5,0.7-0.8,1.5-1,2.5   c-0.2,0.9-0.3,1.9-0.3,2.9c0,1,0.1,1.9,0.3,2.9c0.2,0.9,0.5,1.8,1,2.5c0.5,0.7,1.1,1.3,1.8,1.7c0.8,0.4,1.7,0.7,2.7,0.7   C116.2,50.6,117.1,50.4,117.8,50z"/>
-                                                  <path class="st0" d="M139.3,21.5V55h-6.7V21.5H139.3z"/>
-                                                  <path class="st0" d="M145.7,27v-5.5h6.7V27H145.7z M152.3,30.7V55h-6.7V30.7H152.3z"/>
-                                                  <path class="st0" d="M156,35.2v-4.5h4v-1.9c0-2.2,0.7-3.9,2-5.3c1.3-1.4,3.4-2.1,6.1-2.1c0.6,0,1.2,0,1.8,0.1   c0.6,0,1.2,0.1,1.7,0.1v5c-0.8-0.1-1.6-0.1-2.4-0.1c-0.9,0-1.6,0.2-1.9,0.6c-0.4,0.4-0.6,1.1-0.6,2.1v1.5h4.6v4.5h-4.6V55H160V35.2   H156z"/>
-                                                  <path class="st0" d="M182.2,49.2c1,1,2.4,1.5,4.3,1.5c1.3,0,2.5-0.3,3.5-1c1-0.7,1.6-1.4,1.8-2.1h5.9c-0.9,2.9-2.4,5-4.3,6.2   c-1.9,1.3-4.3,1.9-7,1.9c-1.9,0-3.6-0.3-5.2-0.9c-1.5-0.6-2.8-1.5-3.9-2.6c-1.1-1.1-1.9-2.5-2.5-4c-0.6-1.6-0.9-3.3-0.9-5.2   c0-1.8,0.3-3.5,0.9-5.1c0.6-1.6,1.4-2.9,2.5-4.1c1.1-1.1,2.4-2,3.9-2.7c1.5-0.7,3.2-1,5-1c2.1,0,3.9,0.4,5.4,1.2   c1.5,0.8,2.8,1.9,3.8,3.2c1,1.3,1.7,2.9,2.1,4.6c0.4,1.7,0.6,3.5,0.5,5.4h-17.5C180.7,46.7,181.2,48.2,182.2,49.2z M189.8,36.4   c-0.8-0.9-2-1.3-3.6-1.3c-1.1,0-1.9,0.2-2.6,0.5c-0.7,0.4-1.3,0.8-1.7,1.3c-0.4,0.5-0.7,1.1-0.9,1.7c-0.2,0.6-0.3,1.1-0.3,1.6h10.8   C191.1,38.6,190.5,37.3,189.8,36.4z"/>
-                                                  </g>
-                                                </svg>
-                                              </a>
-                                            </div>
-
-                                          </div>
-                                        </div> <!-- / .row -->
-                                        <img src="/img/splife-ui-min.png" alt="Splife UI" class="img-fluid">
-                                        <!-- Text -->
-                                        <p class="mb-5 mt-5 mb-md-7">
-                                          We completely rebuilt their EmberJS frontend in 5 billable developer weeks.
-
-                                          Using the software methodology of Test Driven Development, we kept a high velocity, while delivering value and ensuring that the new software would work every single time.
-                                        </p>
-
-                                        <!-- Footer -->
-                                        <footer class="blockquote-footer">
-                                          <span class="h6 text-uppercase">Technologies: EmberJS</span>
-                                        </footer>
-
-                                      </blockquote>
-
-                                  </div>
-
                                 </div>
-
-
-
                               </div>
                             </div> <!-- / .row -->
                           </div>
@@ -492,8 +339,7 @@
             <div class="lead text-muted-80 mb-0">
 
               In October 2019, Joshua Plicque left a Software Engineering Lead position to pursue his personal passion of day trading in the stock market.
-              Along the way, in January 2020, he found himself blessed with opportunities to solve three company's business problems through software. Or Equals was born.
-
+              Along the way, in January 2020, he found his phone chirping with opportunities to solve three company's business problems through software. With this much opportunity, Or Equals was born.
               <br>
               <br>
 
