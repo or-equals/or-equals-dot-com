2f6cd6b248d1566d9a01c05e0b722a52bbe79ae0
Joshua Plicque <plicjo@gmail.com>
2025-03-30 15:31:56 -0400

    Update zencase video

--- src/App.svelte
+++ src/App.svelte
@@ -288,7 +288,7 @@
                             <div class="flex flex-col gap-8 ">
                               <h2 class="sm:text-3xl font-black text-charcoal">ZenCase</h2>
                               <div class="object-cover">
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/wtIeuJwro6g?start=17" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                <iframe width="560" height="315" src="https://www.youtube.com/watch?v=N9uJpRVmct4&t=2s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                               </div>
                               <div class="flex flex-col gap-6">
                                 <p class="sm:text-xl font-thin text-charcoal leading-loose">
